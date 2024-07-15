8152f2f957097c830549bf17e4d3d1fe6faba5b1
Xavier6B3 <chavi.jtr10@gmail.com>
2024-07-15 08:04:03 -0500

    Creación del Proyecto Final

---
+++ ProyectoFinalProductos/src/main/java/ec/edu/uce/interfaz/InterfazApplication.java
@@ -0,0 +1,13 @@
+package ec.edu.uce.interfaz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InterfazApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InterfazApplication.class, args);
+	}
+
+}
