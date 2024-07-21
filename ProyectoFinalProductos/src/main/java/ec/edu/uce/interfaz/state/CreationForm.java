a9e02c719c533fe3180e43861ea7662366dec806
Rensso26 <135520250+Rensso26@users.noreply.github.com>
2024-07-21 06:32:56 -0500

    Add files via upload

---
+++ ProyectoFinalProductos/src/main/java/ec/edu/uce/interfaz/state/CreationForm.java
@@ -0,0 +1,59 @@
+package ec.edu.uce.interfaz.state;
+
+import jakarta.persistence.*;
+
+@Entity
+public class CreationForm {
+
+    @Id
+    @Column(unique = true, nullable = false)
+    private int idToy;
+
+    private double timeEnsable;
+    private double timePinter;
+    private double timePackaging;
+
+    @OneToOne
+    @JoinColumn(name = "idToy")
+    private Toy toy;
+
+    public int getIdToy() {
+        return idToy;
+    }
+
+    public void setIdToy(int idToy) {
+        this.idToy = idToy;
+    }
+
+    public double getTimeEnsable() {
+        return timeEnsable;
+    }
+
+    public void setTimeEnsable(double timeEnsable) {
+        this.timeEnsable = timeEnsable;
+    }
+
+    public double getTimePinter() {
+        return timePinter;
+    }
+
+    public void setTimePinter(double timePinter) {
+        this.timePinter = timePinter;
+    }
+
+    public double getTimePackaging() {
+        return timePackaging;
+    }
+
+    public void setTimePackaging(double timePackaging) {
+        this.timePackaging = timePackaging;
+    }
+
+    public Toy getToy() {
+        return toy;
+    }
+
+    public void setToy(Toy toy) {
+        this.toy = toy;
+    }
+}
