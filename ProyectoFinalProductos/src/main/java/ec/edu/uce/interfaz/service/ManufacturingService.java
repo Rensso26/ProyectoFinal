0403ef1a7bcf1fa0baf30e968c46639c7e2f5ed3
red91shadow <joshua-lunag@hotmail.com>
2024-07-28 10:21:29 -0500

    Update ManufacturingService

--- ProyectoFinalProductos/src/main/java/ec/edu/uce/interfaz/service/ManufacturingService.java
+++ ProyectoFinalProductos/src/main/java/ec/edu/uce/interfaz/service/ManufacturingService.java
@@ -1,10 +1,13 @@
 package ec.edu.uce.interfaz.service;
 
-import ec.edu.uce.interfaz.state.Toy;
 import ec.edu.uce.interfaz.repository.ToyRepository;
+import ec.edu.uce.interfaz.state.Toy;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Mono;
+
+import java.util.concurrent.CompletableFuture;
 
 @Service
 public class ManufacturingService {
@@ -12,23 +15,22 @@ public class ManufacturingService {
     @Autowired
     private ToyRepository toyRepository;
 
-    public Mono<String> fabricateToy(Long toyId) {
-        return Mono.fromCallable(() -> {
-            Toy toy = toyRepository.findById(Math.toIntExact(toyId))
-                    .orElseThrow(() -> new RuntimeException("Toy not found"));
-
-            double totalFabricationTime = (double) (toy.getCreationForm().getTimeEnsable()
-                                + toy.getCreationForm().getTimePinter()
-                                + toy.getCreationForm().getTimePackaging());
-
+    @Async
+    public CompletableFuture<String> fabricateToyAsync(Long toyId) {
+        return CompletableFuture.supplyAsync(() -> {
+            Toy toy = toyRepository.findById(Math.toIntExact(toyId)).orElseThrow(() -> new RuntimeException("Toy not found"));
+            double totalTime = toy.getCreationForm().getTimeEnsable() + toy.getCreationForm().getTimePinter() +
+                    toy.getCreationForm().getTimePackaging();
             try {
-                Thread.sleep((long) (totalFabricationTime * 1000L));
+                Thread.sleep((long) (totalTime * 1000L)); // Emulate manufacturing time
             } catch (InterruptedException e) {
                 Thread.currentThread().interrupt();
-                throw new RuntimeException("Fabrication process was interrupted", e);
             }
-
-            return "Toy " + toy.getName() + " fabricated in " + totalFabricationTime + " seconds";
+            return toy.getName() + " fabricated in " + totalTime + " seconds";
         });
+    }
+
+    public Mono<String> fabricateToy(Long toyId) {
+        return Mono.fromFuture(fabricateToyAsync(toyId));
     }
 }
