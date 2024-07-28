b9d99a5cfb8b51d4a7415702d5178dec94f1cc99
red91shadow <joshua-lunag@hotmail.com>
2024-07-28 02:17:40 -0500

    ligeros cambios

--- ProyectoFinalProductos/src/main/java/ec/edu/uce/interfaz/repository/CategoryRepository.java
+++ ProyectoFinalProductos/src/main/java/ec/edu/uce/interfaz/repository/CategoryRepository.java
@@ -2,7 +2,10 @@ package ec.edu.uce.interfaz.repository;
 
 import ec.edu.uce.interfaz.state.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-public interface CategoryRepository extends JpaRepository<Category, String> {
+@Repository
+public interface CategoryRepository extends JpaRepository<Category, Long> {
     Category findByName(String name);
+    void deleteByName(String name);
 }
